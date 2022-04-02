566ef89d0e132883ff0758ec4585ef63f2519684
dhineshL <dhineshdl22@gmail.com>
2022-04-02 12:55:01 +0530

    Spring Security Tutorial FollowUps - Form based authentication

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 import static com.example.demo.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.demo.security.ApplicationUserRole.*;
 
@@ -34,22 +37,33 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("./")
-                .permitAll()
+                    .antMatchers("./")
+                    .permitAll()
                 .antMatchers("/api/**")
-                .hasRole(STUDENT.name())
-//                .antMatchers(HttpMethod.DELETE,"management/api/**").hasAnyAuthority(COURSE_WRITE.name())
-//                .antMatchers(HttpMethod.POST,"management/api/**").hasAnyAuthority(COURSE_WRITE.name())
-//                .antMatchers(HttpMethod.PUT,"management/api/**").hasAnyAuthority(COURSE_WRITE.name())
-//                .antMatchers(HttpMethod.GET,"management/api/**").hasAnyRole(ADMINTRAINEE.name(), ADMIN.name())
+                    .hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login");
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/courses",true)
+                    .passwordParameter("password")
+                    .usernameParameter("username")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(20))
+                    .key("securedKey")
+                    .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login");
     }
 
     @Override
